613b78fb02528084c4f6d9fd1025bce93c1121c5
dev <dhirendhanani@gmail.com>
2017-02-27 15:13:59 +0800

    testing yo

--- app/src/main/java/com/example/dev/testproject/MainActivity.java
+++ app/src/main/java/com/example/dev/testproject/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.dev.testproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_fire;
+    DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_fire = (Button) findViewById(R.id.btn_firebase);
+
+//        if(!FirebaseApp.getApps(this).isEmpty()) {
+//            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+//        }
+        btn_fire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User  u = new User("Jack",33);
+                mDatabase = FirebaseDatabase.getInstance().getReference();
+                mDatabase.child("users").child("3").setValue(u);
+
+            }
+        });
+
     }
 }
